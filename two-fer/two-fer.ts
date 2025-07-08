f5aef23ffc6648efbe829b75a1c27d7a5b0066e8
Vilizar Denichin <denichinv@gmail.com>
2025-07-08 17:52:10 +0100

    Initial commit: added solved Exercism exercises

---
+++ two-fer/two-fer.ts
@@ -0,0 +1,12 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(name?:string){
+if(name){
+  return `One for ${name}, one for me.`;
+
+}
+return `One for you, one for me.`;
+}
+
